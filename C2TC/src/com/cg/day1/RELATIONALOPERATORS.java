d3e1a63a4846bc801ce7c151e9507172901c2c40
user <user@Jarsh-pc>
2023-07-28 10:28:16 +0530

    RELATIONAL OPERATORS

---
+++ C2TC/src/com/cg/day1/RELATIONALOPERATORS.java
@@ -0,0 +1,19 @@
+package com.cg.day1;
+
+public class RELATIONALOPERATORS {
+
+	public static void main(String[] args) {
+	       int a=50,b=30;
+	       if(a>b)
+	    	   System.out.println(" a is greater");
+	       else if(a==b)	   
+	    	   System.out.println("a is equal to b");
+	       else if(a<=b)
+	    	   System.out.println("b is greater and equal to a");
+	       else if(a>=b)
+	    	   System.out.println("a is greater and equal to b");
+	       
+
+	}
+
+}
