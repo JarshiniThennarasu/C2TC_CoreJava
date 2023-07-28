ef02f47f7edafb8e4c25964fb29898650a4c88e7
user <user@Jarsh-pc>
2023-07-28 10:19:04 +0530

    ARITHMETIC OPERATORS

--- C2TC/src/com/cg/day1/Operators.java
+++ C2TC/src/com/cg/day1/Operators.java
@@ -3,8 +3,11 @@ package com.cg.day1;
 public class Operators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a=10,b=5;
+		System.out.println("Add " +(a+b));
+		System.out.println("Sub " +(a-b));
+		System.out.println("Mul " +(a*b));
+		System.out.println("Div " +(a/b));
 	}
 
 }
